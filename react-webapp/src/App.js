5492dc01b67dc9f4e58d3de448bd84de678a7f01
Zahra Parhizkari <zarapar64@gmail.com>
2022-10-29 01:32:04 +0200

    Router-dom and navigation is added

--- react-webapp/src/App.js
+++ react-webapp/src/App.js
@@ -1,14 +1,20 @@
 import './style.min.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import HomeView from './views/HomeView';
 import ContactsView from './views/ContactsView';
+import NotFoundView from './views/NotFoundView';
+
 
 
 function App() {
   return (
-    <>
-      <HomeView />
-      <ContactsView />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+        <Route path="/contacts" element={<ContactsView />} />
+        <Route path="*" element={<NotFoundView />} />
+      </Routes>
+     </BrowserRouter>
   );
 }
 
---
+++ react-webapp/src/views/NotFoundView.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFoundView = () => {
+  return (
+    <div>404 - Page Not Found</div>
+  )
+}
+
+export default NotFoundView
