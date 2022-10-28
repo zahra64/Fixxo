4d8e789963c0b306e5ffdbd3f7c320b85c2f8198
Zahra Parhizkari <zarapar64@gmail.com>
2022-10-28 18:51:50 +0200

    test commit

--- react-webapp/src/App.js
+++ react-webapp/src/App.js
@@ -1,9 +1,13 @@
 import './style.min.css';
+import HomeView from './views/HomeView';
+import ContactsView from './views/ContactsView';
+
 
 function App() {
   return (
     <>
-   
+      <HomeView />
+      <ContactsView />
     </>
   );
 }
---
+++ react-webapp/src/views/ContactsView.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContactsView = () => {
+  return (
+    <div>ContactsView</div>
+  )
+}
+
+export default ContactsView
