b3d704e54334eacafc92c64354545967b2dd642e
Zahra Parhizkari <zarapar64@gmail.com>
2022-11-10 20:43:48 +0100

    Footersection.scss and ExternalLinkIcon for footerIcons is also added

--- react-webapp/src/sections/FooterSection.js
+++ react-webapp/src/sections/FooterSection.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import ExternaLinkIcon from '../components/ExternaLinkIcon'
 
 const FooterSection = () => {
   return (
     <footer>
-        <div class="socialmedia">
-            <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
-            <a href="#"><i class="fa-brands fa-instagram"></i></a>
-            <a href="#"><i class="fa-brands fa-twitter"></i></a>
-            <a href="#"><i class="fa-brands fa-google"></i></a>
-            <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+        <div className="socialmedia">
+          <ExternaLinkIcon link="http://facebook.com"  icon="fa-brands fa-facebook-f"/>
+          <ExternaLinkIcon link="http://instagram.com"  icon="fa-brands fa-instagram"/>
+          <ExternaLinkIcon link="http://twitter.com" icon="fa-brands fa-twitter"/>
+          <ExternaLinkIcon link="http://google.com"  icon="fa-brands fa-google"/>
+          <ExternaLinkIcon link="http://linkedin.com" icon="fa-brands fa-linkedin"/>
+
+          {/* <a href="http://facebook.com" target="blank"><i className="fa-brands fa-facebook-f"></i></a> */}
         </div>
         <div>@ 2022 Fixxo. All Rights Reserved</div>
     </footer>
