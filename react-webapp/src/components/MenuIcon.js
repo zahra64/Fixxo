95d505b212524d030c9c35e49e6a8bc9dcaa3040
Zahra Parhizkari <zarapar64@gmail.com>
2022-11-12 01:22:25 +0100

    responsiv design for MainMenuSection

--- react-webapp/src/components/MenuIcon.js
+++ react-webapp/src/components/MenuIcon.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-const MenuIcon = ({link, icon, quantity}) => {
+const MenuIcon = ({link, icon, quantity, hideOnMobile, hideOnTablet}) => {
   return (
-    <NavLink className="menu-icon" to={link} end>
+    // <NavLink className="menu-icon" to={link} end>
+   
+   <NavLink className={`menu-icon ${hideOnMobile ? "hidOneMobile" : ""} ${hideOnTablet ? "hideOnTablet" : ""}`}  to={link} end>
       <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-theme">{quantity}</span>
       <i className={icon}></i>
     </NavLink>
