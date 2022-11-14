d5092b3e4ba69bd55d2b5addaae73dee18965f02
Zahra Parhizkari <zarapar64@gmail.com>
2022-11-14 14:41:09 +0100

    add button and useSate on MainMenuSection.js

--- react-webapp/src/components/MenuIcon.js
+++ react-webapp/src/components/MenuIcon.js
@@ -5,7 +5,7 @@ const MenuIcon = ({link, icon, quantity, hideOnMobile, hideOnTablet}) => {
   return (
     // <NavLink className="menu-icon" to={link} end>
    
-   <NavLink className={`menu-icon ${hideOnMobile ? "hidOneMobile" : ""} ${hideOnTablet ? "hideOnTablet" : ""}`}  to={link} end>
+   <NavLink className={`menu-icon ${hideOnMobile ? "d-none d-md-flex" : ""} ${hideOnTablet ? "hideOnTablet" : ""}`}  to={link} end>
       <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-theme">{quantity}</span>
       <i className={icon}></i>
     </NavLink>
