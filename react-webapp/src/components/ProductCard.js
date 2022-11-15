a6d89b7e9ee5db47eab9baaccfd4045cd4e67c0d
Zahra Parhizkari <zarapar64@gmail.com>
2022-11-15 17:28:09 +0100

    _productGridSection.scss is added

--- react-webapp/src/components/ProductCard.js
+++ react-webapp/src/components/ProductCard.js
@@ -2,32 +2,32 @@ import React from 'react'
 
 const productCard = () => {
   return (
-    <div class="col">
-        <div class="card">
-            <div class="img">
+    <div className="col">
+        <div className="card">
+            <div className="card-img">
                     <img src="assets/images/imges.jpg" alt="images" />
-                <div class="card-menu">
-                    <a href="#"><i class="fa-regular fa-repeat"></i></a>
-                    <a href="#"><i class="fa-regular fa-heart"></i></a>
-                    <a href="#"><i class="fa-regular fa-bag-shopping"></i></a>
+                <div className="card-menu">
+                    <a href="#"><i className="fa-regular fa-repeat"></i></a>
+                    <a href="#"><i className="fa-regular fa-heart"></i></a>
+                    <a href="#"><i className="fa-regular fa-bag-shopping"></i></a>
                 </div>
-                <a class="_btn-theme" href="#">
-                    <span class="_btn-theme-left"></span>
+                <a className="_btn-theme btn-card-theme" href="#">
+                    <span className="_btn-theme-left"></span>
                     QUICK VIEW
-                    <span class="_btn-theme-right"></span>
+                    <span className="_btn-theme-right"></span>
                 </a>
             </div>
-            <div class="card-body">
-                <p class="card-category">Category</p>
-                <h5 class="card-title">Modern Black Blouse</h5>  
-                <p class="card-rating">
-                    <i class="fa-sharp fa-solid fa-star"></i>
-                    <i class="fa-sharp fa-solid fa-star"></i>
-                    <i class="fa-sharp fa-solid fa-star"></i>
-                    <i class="fa-sharp fa-solid fa-star"></i>
-                    <i class="fa-sharp fa-solid fa-star"></i>
+            <div className="card-body">
+                <p className="card-category">Category</p>
+                <h5 className="card-title">Modern Black Blouse</h5>  
+                <p className="card-rating">
+                    <i className="fa-sharp fa-solid fa-star"></i>
+                    <i className="fa-sharp fa-solid fa-star"></i>
+                    <i className="fa-sharp fa-solid fa-star"></i>
+                    <i className="fa-sharp fa-solid fa-star"></i>
+                    <i className="fa-sharp fa-solid fa-star"></i>
                 </p>
-                <p class="card-price">$35.00</p>  
+                <p className="card-price">$35.00</p>  
             </div>
         </div>
     </div>
--- react-webapp/src/sections/ProductGridSection.js
+++ react-webapp/src/sections/ProductGridSection.js
@@ -3,9 +3,9 @@ import ProductCard from '../components/ProductCard'
 
 const ProductGridSection= () => {
   return (
-    <section class="_featured-products">
-        <div class="container">
-            <div class="row row-cols-1 row-cols-md-4 g-4">
+    <section className="product-grid">
+        <div className="container">
+            <div className="row row-cols-1 row-cols-md-4 g-4">
               <ProductCard />
             </div>
         </div>
