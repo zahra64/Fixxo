17164a79f5fe99165ad7dfa64faddf493294c1c9
Zahra Parhizkari <zarapar64@gmail.com>
2022-11-16 14:17:17 +0100

    adding images with Url

--- react-webapp/src/components/ProductCard.js
+++ react-webapp/src/components/ProductCard.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import TempImg from '../assets/images/imges.jpg'
 
-const productCard = () => {
+const productCard = ({item}) => {
   return (
     <div className="col">
         <div className="card">
             <div className="card-img">
-            <img src={TempImg} alt="" />
+            {/* <img src={TempImg} alt="" /> */}
+                <img src={item.img} alt={item.name} />
                     {/* <img src="assets/images/imges.jpg" alt="images" /> */}
                 <div className="card-menu">
                     <button className="menu-link"><i className="fa-regular fa-heart"></i></button>
@@ -22,8 +23,10 @@ const productCard = () => {
                 </NavLink>
             </div>
             <div className="card-body">
-                <p className="card-category">Category</p>
-                <h5 className="card-title">Modern Black Blouse</h5>  
+                {/* <p className="card-category">Category</p> */}
+                {/* <h5 className="card-title">Modern Black Blouse</h5> */}
+                <p className="card-category">{item.category}</p> 
+                <h5 className="card-title">{item.name}</h5>  
                 <p className="card-rating">
                     <i className="fa-sharp fa-solid fa-star"></i>
                     <i className="fa-sharp fa-solid fa-star"></i>
@@ -31,7 +34,7 @@ const productCard = () => {
                     <i className="fa-sharp fa-solid fa-star"></i>
                     <i className="fa-sharp fa-solid fa-star"></i>
                 </p>
-                <p className="card-price">$35.00</p>  
+                <p className="card-price">{item.price}</p>  
             </div>
         </div>
     </div>
--- react-webapp/src/sections/ProductGridSection.js
+++ react-webapp/src/sections/ProductGridSection.js
@@ -1,20 +1,25 @@
 import React from 'react'
+import { useState } from 'react'
 import ProductCard from '../components/ProductCard'
 
 const ProductGridSection= ({title}) => {
+
+  const [products, setProducts] = useState([
+    {id:1, name:"Modern Black Blouse", category: "Fashion", price :"$35.00", rating: 5, img:"https://images.pexels.com/photos/1600833/pexels-photo-1600833.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"},
+    {id:2, name:"Modern Black Blouse", category: "Fashion", price :"$35.00", rating: 5, img:"https://images.pexels.com/photos/1187952/pexels-photo-1187952.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"},
+    {id:3, name:"Modern Black Blouse", category: "Fashion", price :"$35.00", rating: 5, img:"https://images.pexels.com/photos/11465211/pexels-photo-11465211.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"},
+    {id:4, name:"Modern Black Blouse", category: "Fashion", price :"$35.00", rating: 5, img:"https://images.pexels.com/photos/3755022/pexels-photo-3755022.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"}
+  ])
   return (
     <section className="product-grid">
         <div className="container">
             <h1>{title}</h1>
             <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4">
-              <ProductCard />
-              <ProductCard />
-              <ProductCard />
-              <ProductCard />
-              <ProductCard />
-              <ProductCard />
-              <ProductCard />
-              <ProductCard />
+              {
+                products.map(product => <ProductCard item ={product} />)
+              }
+              
+         
             </div>
         </div>
  </section>
