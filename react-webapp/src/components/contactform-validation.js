c8b3475ba0376fafd575130884a8d9308b377ba3
Zahra Parhizkari <zarapar64@gmail.com>
2022-11-20 22:43:47 +0100

    contactform-validation.js is added and some changes is done on validation

---
+++ react-webapp/src/components/contactform-validation.js
@@ -0,0 +1,49 @@
+export const validate = (e, form =null) => {
+    if (e.type === 'submit') {
+        const errors = {}
+        errors.name = validate_name(form.name)
+        errors.email = validate_email(form.email)
+        errors.comments =validate_comments(form.comments)
+        return errors
+
+    } else {
+        const{id, value} = e.target
+        switch (id) {
+            case 'name':
+                return validate_name(value)
+            case 'email':
+                return validate_email(value)
+            case 'comments':
+                return validate_comments(value)
+        }
+    }
+}
+
+const validate_name = (value) => {
+    if (!value)
+        return 'A name is required'
+    else if (value.length < 2)
+        return 'Must be a valid name'
+    else
+    return null
+}
+
+const validate_email = (value) => {
+    const regex_email = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+    if (!value)
+        return 'An email address is required'
+    else if (!regex_email.test(value))
+        return 'Must be a valid email (eg. name@domail.com)'
+    else
+    return null
+}
+
+const validate_comments = (value) => {
+    if (!value)
+        return 'A comment is required'
+    else if (value.length < 5)
+        return 'Your comment must be at least 5 character long'
+    else
+    return null
+}
