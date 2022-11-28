5ad79f9d6f0f6fa0bcf4860172593ca1fa6a6613
Zahra Parhizkari <zarapar64@gmail.com>
2022-11-28 01:12:43 +0100

    corrections

--- react-webapp/src/sections/FlashSaleSection.js
+++ react-webapp/src/sections/FlashSaleSection.js
@@ -6,43 +6,38 @@ const FlashSaleSection = ({items = []}) => {
   return (
    
     <section className="flashsale container">
-    <div className="flashsale-box">
-        <h1 className="flashsale-title">2 FOR USD $29</h1>
-        <Link to="/products" className="flashsale-button  btn-sale">
-            <span className="corner-left "></span>
-            FLASE SALE
-            <span className="corner-right"></span>
-        </Link>
-    </div>
-    <div className="this-product-grid">
-        <div className="row row-cols-1 row-cols-md-2 g-4" style={{width:"70%"}}>
-        {/* <div className="row row-cols-1 row-cols-md-2 g-4" > */}
-            {
-                items.map( product => <ProductCard key={product.articleNumber} item ={product} />)
-            }
+        <div className="flashsale-box">
+            <h1 className="flashsale-title">2 FOR USD $29</h1>
+            <Link to="/products" className="flashsale-button  btn-sale">
+                <span className="corner-left "></span>
+                FLASE SALE
+                <span className="corner-right"></span>
+            </Link>
         </div>
-    </div>
-
-
-    <div className="this-product-grid">
-        <div className="row row-cols-1 row-cols-md-2 g-4" style={{width:"70%"}}>
-        {/* <div className="row row-cols-1 row-cols-md-2 g-4" > */}
-            {
-                items.map( product => <ProductCard key={product.articleNumber} item ={product} />)
-            }
+        <div className="this-product-grid">
+            <div className="row row-cols-1 row-cols-md-2 g-4" style={{width:"70%"}}>
+            {/* <div className="row row-cols-1 row-cols-md-2 g-4" > */}
+                {
+                    items.map( product => <ProductCard key={product.articleNumber} item ={product} />)
+                }
+            </div>
         </div>
-    </div>
-    <div className="flashsale-box">
-        <h1 className="flashsale-title">2 FOR USD $29</h1>
-        <Link to="/products" className="flashsale-button  btn-sale">
-            <span className="corner-left "></span>
-            FLASE SALE
-            <span className="corner-right"></span>
-        </Link>
-    </div>
-   
-
-</section>
+        <div className="this-product-grid">
+            <div className="row row-cols-1 row-cols-md-2 g-4" style={{width:"70%", position:'relative', left:'250px'}}>
+                {
+                    items.map( product => <ProductCard key={product.articleNumber} item ={product} />)
+                }
+            </div>
+        </div>
+        <div className="flashsale-box">
+            <h1 className="flashsale-title">2 FOR USD $29</h1>
+            <Link to="/products" className="flashsale-button  btn-sale">
+                <span className="corner-left "></span>
+                FLASE SALE
+                <span className="corner-right"></span>
+            </Link>
+        </div>
+    </section>
   )
 }
 
